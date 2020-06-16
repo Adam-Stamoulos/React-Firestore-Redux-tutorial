eff4b274bb7db030de3a4546212279915154c5aa
Adam-Stamoulos <61904453+Adam-Stamoulos@users.noreply.github.com>
2020-06-16 10:22:54 +0100

    RouterPages

---
+++ ReactReduxFirestore/revents/src/features/user/userDetailed/UserDetailedPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserDetailedPage = () => {
+    return (
+        <div>
+           <h1>User detaild page</h1> 
+        </div>
+    )
+}
+
+export default UserDetailedPage
