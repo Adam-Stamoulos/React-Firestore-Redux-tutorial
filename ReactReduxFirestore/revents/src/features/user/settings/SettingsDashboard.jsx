10c52fa44213567d5fabab346eaa15e1c3c92523
Adam-Stamoulos <61904453+Adam-Stamoulos@users.noreply.github.com>
2020-07-09 17:05:26 +0100

    End of section 6

--- ReactReduxFirestore/revents/src/features/user/settings/SettingsDashboard.jsx
+++ ReactReduxFirestore/revents/src/features/user/settings/SettingsDashboard.jsx
@@ -7,19 +7,17 @@ import AboutPage from "./AboutPage";
 import PhotosPage from "./PhotosPage";
 import AccountPage from "./AccountPage";
 
-
 const SettingsDashboard = () => {
   return (
     <Grid>
       <Grid.Column width={12}>
-          <Switch>
-              
-          </Switch>
-        <Redirect exact from='/settings' to='/settings/basic' />
-        <Route path='/settings/basic'component={BasicPage}/>
-        <Route path='/settings/about'component={AboutPage}/>
-        <Route path='/settings/photos'component={PhotosPage}/>
-        <Route path='/settings/account'component={AccountPage}/>
+        <Switch>
+          <Redirect exact from='/settings' to='/settings/basic' />
+          <Route path='/settings/basic' component={BasicPage} />
+          <Route path='/settings/about' component={AboutPage} />
+          <Route path='/settings/photos' component={PhotosPage} />
+          <Route path='/settings/account' component={AccountPage} />
+        </Switch>
       </Grid.Column>
       <Grid.Column width={4}>
         <SettingsNav />
